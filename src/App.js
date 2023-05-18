56d0c36d53414555304d7c2157bbedabe82903f5
rafael.lumang <lumang.rafael@gmail.com>
2023-05-18 16:12:54 -0700

    moves axios to handleQuwry function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import axios from 'axios';
 
 // App components
@@ -13,9 +13,14 @@ import './css/index.css';
 
 function App() {
   const [photos, setPhotos] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("cats");
 
   useEffect(() => {
+    handleQueryChange(query)
+  }, [query]);
+
+  const handleQueryChange = searchText => {
+    setQuery(searchText);
     // Axios
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => {
@@ -26,10 +31,6 @@ function App() {
         // handle error
         console.log("Error fetching and parsing the data", error);
       })
-  }, [query]);
-
-  const handleQueryChange = searchText => {
-    setQuery(searchText);
   }
 
 
@@ -38,8 +39,9 @@ function App() {
       <SearchForm changeQuery={handleQueryChange} />
       <Nav />
       <Routes>
-        <Route path="cats" element={<PhotoContainer data={photos} query={"cats"} />} />
-        <Route path="dogs" />
+        <Route path="/" element={<Navigate to={'/cats'} />} />
+        <Route path="cats" />
+        <Route path="dogs"/>
         <Route path="computers" />
       </Routes>
       <PhotoContainer data={photos} query={query} />
